291663820dd9fcb6e811b8d1e7b9cf1c9092e736
liamwm <liam.wm1@gmail.com>
2024-08-05 11:43:26 +0000

    more palette styling

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { useEffect, useState, useRef } from 'react';
 
 function App() {
   const [squareValues, setSquareValues] = useState(Array(81).fill(" "));
-  const [brushValue, setBrushValue] = useState("9");
+  const [brushValue, setBrushValue] = useState("1");
   const [status, setStatus] = useState("");
   const swipl = useRef();
-  const paletteOptions = ([...Array(9).keys()].map(x => x.toString()));
+  const paletteOptions = ([...Array(9).keys()].map(x => (x+1).toString()));
   paletteOptions.push("Erase");
 
   const preparePuzzleInput = (squareValues) => {
@@ -69,27 +69,25 @@ function App() {
  
   return (
     <div className="App">
-        <div className="grid grid-cols-9 size-fit gap-1">
+        <div className="grid grid-cols-9 size-fit gap-0.5">
           {[...Array(squareValues.length).keys()].map(x => 
             <div className="size-10 bg-sky-500 hover:bg-sky-700 place-content-center" key={x} onClick={() => onClickPuzzleCell(x)}>
               <p className="text-center">{squareValues[x]}</p>
             </div>
           )}
         </div>
-        <div className="grid grid-cols-5 size-fit">
+        <div className="grid grid-cols-5 gap-0.5 size-fit border-solid border-gray-300 border-2 rounded-lg overflow-hidden">
           {paletteOptions.map(x =>
-            <div key={x}>
-              <input type="radio" id={x} name="editor-palette" className="hidden peer" onChange={(e) => onChangeNumberRadio(e, x)}/>
-              <label className="inline-flex bg-red-300 hover:bg-red-400 peer-checked:bg-red-500" htmlFor={x}>
-                <div className="size-16 place-content-center">
-                  {x === "Erase" ? <img src="assets/eraser.svg" className="p-5"/> : <p className="text-center">{x}</p>}
-                </div>
+            <div className="size-16 bg-red-300 hover:bg-red-400 has-[:checked]:bg-red-500" key={x}>
+              <input type="radio" id={x} name="editor-palette" className="hidden peer" onChange={(e) => onChangeNumberRadio(e, x)} checked={x === brushValue}/>
+              <label className="block h-full content-center" htmlFor={x}>
+                  {x === "Erase" ? <img src="assets/eraser.svg" alt="Eraser" className="p-5"/> : <p className="text-center">{x}</p>}
               </label>
             </div>
           )}
         </div>
-        <button type="button" onClick={solvePuzzle}>Solve!</button>
-        <button type="button" onClick={resetPuzzle}>Reset</button>
+        <button type="button" className="rounded-full bg-blue-200" onClick={solvePuzzle}>Solve!</button>
+        <button type="button" className="rounded-full bg-slate-200" onClick={resetPuzzle}>Reset</button>
         <div className="solverStatus">{status}</div>
     </div>
   );
